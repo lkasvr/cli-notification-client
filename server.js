2922cdbfb2ddfe9a7e9a3471e4bc9654f1e228ca
helbert.rios <helbert.rios@philips.com>
2023-04-13 21:45:48 -0300

    feat(notification): Partial websocket log.

--- server.js
+++ server.js
@@ -11,25 +11,24 @@ const port = 3080;
 // A simple dataSource that changes over time
 const requestListener = (request, response) => {
     console.log(`server.js: Enter in requestListener -> ${request.url}`);
-  if (request.url == '/' || request.url === '/notification' || request.url === '/notification.html') {
+    if (request.url == '/' || request.url === '/notification' || request.url === '/notification.html') {
         fs.readFile("notification.html", (error, data) => actionExecute( { error: error, data: data, response: response, contentType: 'text/html'}));
-  } else if (request.url === '/modal-channel.html') {
-      fs.readFile("modal-subscrible.html", (error, data) => actionExecute( { error: error, data: data, response: response, contentType: 'text/html'}))
-  } else if (request.url === '/modal-connection.html') {
+    } else if (request.url === '/modal-channel.html') {
+        fs.readFile("modal-subscrible.html", (error, data) => actionExecute( { error: error, data: data, response: response, contentType: 'text/html'}))
+    } else if (request.url === '/modal-connection.html') {
         fs.readFile("modal-connection.html", (error, data) => actionExecute( { error: error, data: data, response: response, contentType: 'text/html'}));
-  } else if (request.url === '/websocket.js') {
+    } else if (request.url === '/websocket.js') {
         fs.readFile("websocket.js", (error, data) => actionExecute( { error: error, data: data, response: response, contentType: 'text/javascript'}));
-  } else if (request.url === '/console.js') {
+    } else if (request.url === '/console.js') {
         fs.readFile("console.js", (error, data) => actionExecute( { error: error, data: data, response: response, contentType: 'text/javascript'}));
-  } else if (request.url === '/logger.js') {
+    } else if (request.url === '/logger.js') {
         fs.readFile("logger.js", (error, data) => actionExecute( { error: error, data: data, response: response, contentType: 'text/javascript'}));
-  } else if (request.url === '/notification.css') {
+    } else if (request.url === '/notification.css') {
         fs.readFile("notification.css", (error, data) => actionExecute( { error: error, data: data, response: response, contentType: 'text/css'}));
-  } else {
-    console.log(`server.js: The resource ${request.url} does not found`);
-        actionExecute({error: `The resource ${request.url} does not exist`, response: response});
-  }
-
+    } else {
+        console.log(`server.js: The resource ${request.url} does not found`);
+        fs.readFile("404.html", (error, data) => actionExecute( { error: error, data: data, response: response, contentType: 'text/html'}));
+    }
 };
 
 actionExecute = function (action) {
@@ -40,10 +39,6 @@ actionExecute = function (action) {
     }
     action.response.end(action.error);
 };
-
-
-
-
 
 const server = http.createServer(requestListener);
 server.listen(port, host, () => console.log(`server.js: server running at http://${host}:${port}`));
